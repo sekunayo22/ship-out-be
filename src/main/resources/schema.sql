7481a9ae20155fa16aa65e74ac3d4e6c0a54ea46
sekunayo22 <graceokereke22@gmail.com>
2026-01-20 22:02:56 +0000

    mappings

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,6 @@ CREATE SEQUENCE IF NOT EXISTS booking_master_bill_seq;
 CREATE SEQUENCE IF NOT EXISTS booking_carrier_contract_seq;
 
 ALTER TABLE IF EXISTS booking
-    ALTER COLUMN IF EXISTS reference SET DEFAULT nextval('booking_reference_seq'),
-    ALTER COLUMN IF EXISTS master_bill SET DEFAULT nextval('booking_master_bill_seq'),
-    ALTER COLUMN IF EXISTS carrier_contract_number SET DEFAULT nextval('booking_carrier_contract_seq');
+    ALTER COLUMN reference SET DEFAULT nextval('booking_reference_seq'),
+    ALTER COLUMN master_bill SET DEFAULT nextval('booking_master_bill_seq'),
+    ALTER COLUMN carrier_contract_number SET DEFAULT nextval('booking_carrier_contract_seq');
