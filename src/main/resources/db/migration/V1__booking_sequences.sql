1ea5663c70c8bfa75567bddc5be365a82325eae1
sekunayo22 <graceokereke22@gmail.com>
2026-01-21 00:09:52 +0000

    database migrations

--- src/main/resources/db/migration/V1__booking_sequences.sql
+++ src/main/resources/db/migration/V1__booking_sequences.sql
@@ -5,4 +5,4 @@ CREATE SEQUENCE IF NOT EXISTS booking_carrier_contract_seq;
 ALTER TABLE IF EXISTS booking
     ALTER COLUMN reference SET DEFAULT nextval('booking_reference_seq'),
     ALTER COLUMN master_bill SET DEFAULT nextval('booking_master_bill_seq'),
-    ALTER COLUMN carrier_contract_number SET DEFAULT nextval('booking_carrier_contract_seq');
+    ALTER COLUMN carrier_contract_number SET DEFAULT nextval('booking_carrier_contract_seq')
